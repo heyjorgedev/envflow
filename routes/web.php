ab5c4c6ed7c405fc3827f910f2f1b523397be8df
Jorge Lapa <2780099+heyjorgedev@users.noreply.github.com>
2025-01-05 18:13:41 +0700

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\EnvController;
 use App\Livewire\EncryptEnvPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', EncryptEnvPage::class);
-
-Route::resource('envs', EnvController::class)
-    ->only('store', 'show');
+Route::get('/success/{id}', EncryptEnvPage::class);

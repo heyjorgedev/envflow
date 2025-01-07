bf9e74dc602269ee84957ef8dd168f5b2bc2f588
Jorge Lapa <2780099+heyjorgedev@users.noreply.github.com>
2025-01-07 20:12:24 +0700

    fix: dark mode header

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -18,7 +18,7 @@
             Envflow
         </a>
         <div class="flex lg:hidden">
-            <button x-on:click="open = true" type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+            <button x-on:click="open = true" type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 dark:text-white">
                 <span class="sr-only">Open main menu</span>
                 <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -37,7 +37,7 @@
     <div x-cloak x-show="open" class="lg:hidden" role="dialog" aria-modal="true">
         <!-- Background backdrop, show/hide based on slide-over state. -->
         <div class="fixed inset-0 z-10"></div>
-        <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-slate-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div class="flex items-center justify-between">
                 <a href="{{ route('home') }}" class="-m-1.5 p-1.5 font-bold text-2xl text-rose-600">
                     Envflow
@@ -53,7 +53,7 @@
                 <div class="-my-6 divide-y divide-gray-500/10">
                     <div class="space-y-2 py-6">
                         @foreach ($menus as $menu)
-                            <a href="{{ $menu['url'] }}" @class(['bg-rose-600 text-white' => $menu['active'], 'text-gray-900 hover:bg-gray-50' => !$menu['active'], '-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold']) wire:navigate>
+                            <a href="{{ $menu['url'] }}" @class(['bg-rose-600 text-white' => $menu['active'], 'text-gray-900 dark:text-white hover:bg-gray-50 dark:hover:bg-slate-600' => !$menu['active'], '-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold']) wire:navigate>
                                 {{ $menu['name'] }}
                             </a>
                         @endforeach

9b70424eb211125ada25321aadbb90db8fccb207
Jorge Lapa <2780099+heyjorgedev@users.noreply.github.com>
2025-01-08 20:33:12 +0700

    improve darkmode

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -33,16 +33,14 @@
             @endforeach
         </div>
     </nav>
-    <!-- Mobile menu, show/hide based on menu open state. -->
     <div x-cloak x-show="open" class="lg:hidden" role="dialog" aria-modal="true">
-        <!-- Background backdrop, show/hide based on slide-over state. -->
-        <div class="fixed inset-0 z-10"></div>
+        <div class="fixed inset-0 z-10 bg-black/10 backdrop-blur"></div>
         <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-slate-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div class="flex items-center justify-between">
                 <a href="{{ route('home') }}" class="-m-1.5 p-1.5 font-bold text-2xl text-rose-600">
                     Envflow
                 </a>
-                <button x-on:click="open = false" type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+                <button x-on:click="open = false" type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700 dark:text-white">
                     <span class="sr-only">Close menu</span>
                     <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
